c2152959be966f1a136ab0377be40ac65cbf4d65
Jay Ho Oh <ohjayhoi@gmail.com>
2025-01-04 16:26:45 +0900

    feat: add home view

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import MainHeader from "@/components/MainHeader.vue";
+import Navbar from "@/components/Navbar.vue";
+</script>
+
+<template>
+  <MainHeader />
+  <Navbar />
+</template>
