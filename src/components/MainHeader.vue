311a140a09b9af819cc6f06b8c25861004e1aedb
Jay Ho Oh <ohjayhoi@gmail.com>
2025-01-04 16:26:02 +0900

    feat: add main header component

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+
+<template>
+  <header
+    class="w-[500px] h-[60px] fixed top-0 flex justify-between items-center px-5"
+  >
+    <div>
+      <img src="@/assets/img/logo.png" alt="" class="h-[30px]" />
+    </div>
+    <div class="w-[152.8px] flex justify-between">
+      <button
+        class="w-[102.8px] flex justify-between items-center border border-[#dadce0] rounded-2xl px-[10px] h-8"
+      >
+        <img src="@/assets/img/bubble.png" alt="" class="h-[24px]" />
+        <span class="text-sm">Support</span>
+      </button>
+      <button>
+        <img src="@/assets/img/ko.png" alt="" class="h-[28px]" />
+      </button>
+    </div>
+  </header>
+</template>
