68b806f5bc8b2808c23336d6b2d9176c2289184c
Jay Ho Oh <ohjayhoi@gmail.com>
2025-01-04 16:26:17 +0900

    feat: add navigation bar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<script setup></script>
+
+<template>
+  <footer
+    class="w-[500px] h-[70px] flex fixed bottom-0 border-t border-[#dadce0]"
+  >
+    <button class="w-1/4 h-full flex flex-col items-center justify-center">
+      <img src="@/assets/img/navbar/main.png" alt="" class="w-6 m-1" />
+      <span class="text-xs">Main</span>
+    </button>
+    <button class="w-1/4 h-full flex flex-col items-center justify-center">
+      <img src="@/assets/img/navbar/benefit.png" alt="" class="w-6 m-1" />
+      <span class="text-xs">Benefit</span>
+    </button>
+    <button class="w-1/4 h-full flex flex-col items-center justify-center">
+      <img src="@/assets/img/navbar/inbox.png" alt="" class="w-6 m-1" />
+      <span class="text-xs">Inbox</span>
+    </button>
+    <button class="w-1/4 h-full flex flex-col items-center justify-center">
+      <img src="@/assets/img/navbar/setting.png" alt="" class="w-6 m-1" />
+      <span class="text-xs">Setting</span>
+    </button>
+  </footer>
+</template>
