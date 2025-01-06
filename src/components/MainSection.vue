51c35a38eb13744e4ab0f3e8ac042288c77f5d4d
Jay Ho Oh <ohjayhoi@gmail.com>
2025-01-06 22:57:03 +0900

    feat: add main section

---
+++ src/components/MainSection.vue
@@ -0,0 +1,9 @@
+<script setup>
+import ImageSlider from "./ImageSlider.vue";
+</script>
+
+<template>
+  <div class="pt-[70px] pb-[60px]">
+    <ImageSlider />
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import MainHeader from "@/components/MainHeader.vue";
+import MainSection from "@/components/MainSection.vue";
 import Navbar from "@/components/Navbar.vue";
 </script>
 
 <template>
   <MainHeader />
+  <MainSection />
   <Navbar />
 </template>
