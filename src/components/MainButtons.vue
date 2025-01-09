5cbdf30f3d585cfb6aea019627f6a227cf5f97b6
Jay Ho Oh <ohjayhoi@gmail.com>
2025-01-09 22:27:51 +0900

    feat: add MainButtons component

---
+++ src/components/MainButtons.vue
@@ -0,0 +1,24 @@
+<script setup>
+const btns = ["community", "support_cetner", "shopping", "application_form"];
+
+const getImageUrl = (src) => {
+  const baseUrl = window.location.origin;
+  return new URL(src, baseUrl).href;
+};
+</script>
+
+<template>
+  <section>
+    <ul>
+      <li v-for="btn in btns" :key="btn">
+        <button>
+          <img
+            :src="getImageUrl(`src/assets/img/mainBtns/icon_${btn}.png`)"
+            alt="btn"
+          />
+          <span>{{ btn }}</span>
+        </button>
+      </li>
+    </ul>
+  </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import MainHeader from "@/components/MainHeader.vue";
 import MainSection from "@/components/MainSection.vue";
+import MainButtons from "@/components/MainButtons.vue";
 import Navbar from "@/components/Navbar.vue";
 </script>
 
 <template>
   <MainHeader />
   <MainSection />
+  <MainButtons />
   <Navbar />
 </template>
