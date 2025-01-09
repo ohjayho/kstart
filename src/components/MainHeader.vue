23cf335f0e5e1cddf5cfd964816ebbec0e632d46
Jay Ho Oh <ohjayhoi@gmail.com>
2025-01-09 19:33:19 +0900

    style: replace support buttton with inbox button

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -7,12 +7,9 @@
     <div>
       <img src="@/assets/img/header/logo.png" alt="" class="h-[30px]" />
     </div>
-    <div class="w-[152.8px] flex justify-between">
-      <button
-        class="w-[102.8px] flex justify-between items-center border border-[#dadce0] rounded-2xl px-[10px] h-8"
-      >
-        <img src="@/assets/img/header/bubble.png" alt="" class="h-[24px]" />
-        <span class="text-sm">Support</span>
+    <div class="flex">
+      <button class="flex justify-between items-center mr-3">
+        <img src="@/assets/img/header/inbox.png" alt="btn_inbox" class="w-7" />
       </button>
       <button>
         <img src="@/assets/img/header/ko.png" alt="" class="h-[28px]" />
