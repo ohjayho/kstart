ec36bcbd7fbedec77446117e871384ef9a2a6c10
Jay Ho Oh <ohjayhoi@gmail.com>
2025-01-09 18:49:55 +0900

    chore: change folder directory for better folder structure

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -5,17 +5,17 @@
     class="w-[500px] h-[60px] absolute top-0 z-10 flex justify-between items-center px-5 bg-white"
   >
     <div>
-      <img src="@/assets/img/logo.png" alt="" class="h-[30px]" />
+      <img src="@/assets/img/header/logo.png" alt="" class="h-[30px]" />
     </div>
     <div class="w-[152.8px] flex justify-between">
       <button
         class="w-[102.8px] flex justify-between items-center border border-[#dadce0] rounded-2xl px-[10px] h-8"
       >
-        <img src="@/assets/img/bubble.png" alt="" class="h-[24px]" />
+        <img src="@/assets/img/header/bubble.png" alt="" class="h-[24px]" />
         <span class="text-sm">Support</span>
       </button>
       <button>
-        <img src="@/assets/img/ko.png" alt="" class="h-[28px]" />
+        <img src="@/assets/img/header/ko.png" alt="" class="h-[28px]" />
       </button>
     </div>
   </header>
