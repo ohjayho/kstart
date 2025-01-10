d9705feb9421637730d719653535a171163dfd08
Jay Ho Oh <ohjayhoi@gmail.com>
2025-01-10 22:50:33 +0900

    refactor: add MainSlider for component separation

--- src/components/MainSection.vue
+++ src/components/MainSection.vue
@@ -1,37 +1,13 @@
 <script setup>
 import ImageSlider from "./ImageSlider.vue";
-import { register } from "swiper/element/bundle";
-register();
+import MainButtons from "./MainButtons.vue";
+import ShoppingSlider from "./ShoppingSlider.vue";
 </script>
 
 <template>
-  <div class="pt-[60px] pb-[70px]">
+  <div class="pt-[30px] pb-[70px]">
     <!-- <ImageSlider /> -->
-    <swiper-container
-      pagination="true"
-      autoplay-delay="2000"
-      loop="true"
-      disable-on-interaction="false"
-      style="
-        --swiper-pagination-color: #fff;
-        --swiper-pagination-bullet-size: 5px;
-      "
-    >
-      <swiper-slide>
-        <a href="https://support.kstart.co.kr/ko" target="_blank">
-          <img src="@/assets/img/sliderImg/first.png" alt="First Image" />
-        </a>
-      </swiper-slide>
-      <swiper-slide>
-        <a href="https://support.kstart.co.kr/ko" target="_blank">
-          <img src="@/assets/img/sliderImg/second.png" alt="Second Image" />
-        </a>
-      </swiper-slide>
-      <swiper-slide>
-        <a href="https://support.kstart.co.kr/ko" target="_blank">
-          <img src="@/assets/img/sliderImg/third.png" alt="Third Image" />
-        </a>
-      </swiper-slide>
-    </swiper-container>
+    <MainButtons />
+    <ShoppingSlider />
   </div>
 </template>
---
+++ src/components/MainSlider.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { register } from "swiper/element/bundle";
+register();
+</script>
+
+<template>
+  <swiper-container
+    pagination="true"
+    autoplay-delay="2000"
+    loop="true"
+    disable-on-interaction="false"
+    style="
+      --swiper-pagination-color: #fff;
+      --swiper-pagination-bullet-size: 5px;
+    "
+    class="pt-[60px]"
+  >
+    <swiper-slide>
+      <a href="https://support.kstart.co.kr/ko" target="_blank">
+        <img src="@/assets/img/sliderImg/first.png" alt="First Image" />
+      </a>
+    </swiper-slide>
+    <swiper-slide>
+      <a href="https://support.kstart.co.kr/ko" target="_blank">
+        <img src="@/assets/img/sliderImg/second.png" alt="Second Image" />
+      </a>
+    </swiper-slide>
+    <swiper-slide>
+      <a href="https://support.kstart.co.kr/ko" target="_blank">
+        <img src="@/assets/img/sliderImg/third.png" alt="Third Image" />
+      </a>
+    </swiper-slide>
+  </swiper-container>
+</template>
