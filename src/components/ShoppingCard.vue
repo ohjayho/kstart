435b6efee0937b8c5a3cd8754c826e6c1e23eae0
Jay Ho Oh <ohjayhoi@gmail.com>
2025-01-10 22:52:13 +0900

    feat: add ShoppingCard component for reusability

---
+++ src/components/ShoppingCard.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { getImageUrl } from "@/utils/getImageUrl";
+defineProps({
+  item: Object,
+  index: Number
+});
+</script>
+
+<template>
+  <swiper-slide>
+    <a :href="item.link" target="_blank">
+      <div
+        class="w-[152px] h-[232px] flex flex-col items-center p-2 shadow-[0_4px_12px_#0a0f150a]"
+      >
+        <img :src="getImageUrl(item.src)" alt="" class="w-[100px] h-[100px]" />
+        <div
+          class="card-body h-[86px] flex flex-col justify-between text-[12px]"
+        >
+          <p>{{ item.name }}</p>
+          <div class="w-full flex justify-between text-[14px] font-bold">
+            <span>{{ item.price + "원" }}</span>
+            <span class="text-[#2760EE]">{{ item.discount + "%" }}</span>
+          </div>
+          <div class="flex w-full text-white text-[10px]">
+            <span
+              v-if="item.best"
+              class="w-[41px] h-5 bg-[#2760ee] rounded-xl px-2 py-0.5"
+              >BEST</span
+            >
+            <span
+              v-if="item.new"
+              class="w-[41px] h-5 bg-[#59d683] rounded-xl px-2 py-0.5 ml-1"
+              >NEW</span
+            >
+          </div>
+        </div>
+      </div>
+    </a>
+  </swiper-slide>
+</template>
