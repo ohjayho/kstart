f7f6f3c9b2c62ca64536c8304a288b5c3d11e939
Jay Ho Oh <ohjayhoi@gmail.com>
2025-01-13 23:07:20 +0900

    style: move header and navigation bar to App vue file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,9 @@
 <script setup>
-import MainHeader from "@/components/MainHeader.vue";
 import MainSection from "@/components/MainSection.vue";
 import MainSlider from "@/components/MainSlider.vue";
-import Navbar from "@/components/Navbar.vue";
 </script>
 
 <template>
-  <MainHeader />
   <MainSlider />
   <MainSection />
   <Navbar />
