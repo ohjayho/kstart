9f66664a4277002763ace2dddf1505a845659559
Jay Ho Oh <ohjayhoi@gmail.com>
2025-01-22 23:01:49 +0900

    style: move main header to home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
+import MainHeader from "@/components/MainHeader.vue";
 import MainSection from "@/components/MainSection.vue";
 import MainSlider from "@/components/MainSlider.vue";
 </script>
 
 <template>
+  <MainHeader />
   <MainSlider />
   <MainSection />
 </template>
