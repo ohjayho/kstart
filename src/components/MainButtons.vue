91b933418b775b12e0ac923572884a769970a1da
Jay Ho Oh <ohjayhoi@gmail.com>
2025-01-24 17:18:33 +0900

    feat: add links to main buttons

--- src/components/MainButtons.vue
+++ src/components/MainButtons.vue
@@ -1,19 +1,35 @@
 <script setup>
 import { getImageUrl } from "@/utils/getImageUrl";
-const btns = ["community", "support", "shopping", "application_form"];
+import { RouterLink } from "vue-router";
+const btns = [
+  { name: "support", link: "https://support.kstart.co.kr/ko" },
+  { name: "shopping", link: "https://shop.kstart.kr/" },
+  {
+    name: "application_form",
+    link: "https://support.kstart.co.kr/ko/categories/49351379"
+  }
+];
 </script>
 
 <template>
   <section class="pb-[30px]">
     <ul class="w-full flex justify-between px-5">
+      <li key="community">
+        <RouterLink class="flex flex-col items-center" to="/community">
+          <img src="@/assets/img/mainBtns/icon_community.png" alt="btn" />
+          <span class="capitalize text-xs">Community</span>
+        </RouterLink>
+      </li>
       <li v-for="btn in btns" :key="btn">
-        <button class="flex flex-col items-center">
+        <a class="flex flex-col items-center" :href="btn.link" target="_blank">
           <img
-            :src="getImageUrl(`src/assets/img/mainBtns/icon_${btn}.png`)"
+            :src="getImageUrl(`src/assets/img/mainBtns/icon_${btn.name}.png`)"
             alt="btn"
           />
-          <span class="capitalize text-xs">{{ btn.replace("_", " ") }}</span>
-        </button>
+          <span class="capitalize text-xs">{{
+            btn.name.replace("_", " ")
+          }}</span>
+        </a>
       </li>
     </ul>
   </section>
