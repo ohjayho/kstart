0a83723874ba334f04e4871b6a74d53c633a6c21
Jay Ho Oh <ohjayhoi@gmail.com>
2025-01-26 23:10:51 +0900

    feat: add benefit header

---
+++ src/components/benefit/BenefitHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="benefit-header-container py-5">
+    <header class="flex flex-col-reverse h-[196px] mb-3">
+      <h1
+        class="flex justify-center relative font-extrabold text-4xl text-[#2760ee] z-10"
+      >
+        Grand Opening
+        <img
+          src="@/assets/img/benefit/benefit-opening.png"
+          class="absolute w-[176px] -top-36 -z-10"
+          alt="benefit-opening"
+        />
+      </h1>
+    </header>
+    <h2 class="flex text-center text-[#414549] font-semibold">
+      K start의 회원이 누릴 수 있는 모든 혜택<br />
+      지금 바로 드립니다.
+    </h2>
+  </div>
+</template>
