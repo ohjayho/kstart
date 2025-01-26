a4c034d41fd5fbf014e59da4214b50f6caa34aa5
Jay Ho Oh <ohjayhoi@gmail.com>
2025-01-26 23:10:24 +0900

    feat: add benefit component

---
+++ src/components/benefit/Benefit.vue
@@ -0,0 +1,31 @@
+<script setup>
+import BenefitItem from "./BenefitItem.vue";
+</script>
+
+<template>
+  <div
+    class="benefit-items w-full flex flex-col gap-2 items-center text-white text-lg font-semibold"
+  >
+    <BenefitItem
+      title="Shopping Benefit"
+      :description="[
+        '5,000 KRW Welcome Credit',
+        'Earn 3,000 Coupon on 30,000 KRW+'
+      ]"
+      :style="{ backgroundColor: '#96acf9' }"
+    />
+    <BenefitItem
+      title="Support Benefit"
+      :description="[
+        'Free consultation with a native speaker',
+        '10% discount for first time users'
+      ]"
+      :style="{ backgroundColor: '#f1c075' }"
+    />
+    <BenefitItem
+      title="Exclusive Benefits for 'Kstart' Members!"
+      :description="['The more you use it, the more useful.']"
+      :style="{ backgroundColor: '#b897fd' }"
+    />
+  </div>
+</template>
