c2e3eb60143280b3d40b2dd057c9f789705c7a44
Jay Ho Oh <ohjayhoi@gmail.com>
2025-01-27 11:50:12 +0900

    feat: add benefit image component

---
+++ src/components/benefit/BenefitImage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="benefit-image-container w-full h-[340px] relative">
+    <img
+      src="@/assets/img/benefit/benefit-thunder.png"
+      class="w-[53px] absolute left-[38px] top-0"
+    />
+    <img
+      src="@/assets/img/benefit/benefit-thunder.png"
+      class="w-[53px] absolute right-[15px] top-[25px]"
+    />
+    <img
+      src="@/assets/img/benefit/benefit-thunder.png"
+      class="w-[53px] absolute left-[20px] top-[170px]"
+    />
+    <img
+      src="@/assets/img/benefit/benefit-thunder.png"
+      class="w-[53px] absolute left-[160px] bottom-0"
+    />
+    <img
+      src="@/assets/img/benefit/benefit-megaphone.png"
+      class="w-[157px] absolute right-0 top-[107px]"
+    />
+    <img
+      src="@/assets/img/benefit/benefit-thunders.png"
+      class="w-[48px] absolute right-[151px] top-[80px]"
+    />
+    <span
+      class="absolute top-[112px] left-24 text-[28px] text-[#2760ee] underline font-semibold"
+      >K start 앱<br />소문내 주세요!</span
+    >
+  </div>
+</template>
