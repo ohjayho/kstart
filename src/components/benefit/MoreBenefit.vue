acade1805d19cbc30ee77b33ccece384f475cdc9
Jay Ho Oh <ohjayhoi@gmail.com>
2025-01-27 11:50:44 +0900

    feat: add more benefit component

---
+++ src/components/benefit/MoreBenefit.vue
@@ -0,0 +1,35 @@
+<script setup>
+const moreBenefits = [
+  { name: "쇼핑", image: "src/assets/img/benefit/more-benefit-shopping.png" },
+  {
+    name: "커뮤니티",
+    image: "src/assets/img/benefit/more-benefit-community.png"
+  },
+  {
+    name: "보험",
+    image: "src/assets/img/benefit/more-benefit-insurance.png"
+  },
+  {
+    name: "대출",
+    image: "src/assets/img/benefit/more-benefit-loan.png"
+  }
+];
+</script>
+
+<template>
+  <div
+    class="more-benefit-container flex flex-wrap gap-2 w-full max-w-full mt-5"
+  >
+    <div
+      v-for="moreBenefit in moreBenefits"
+      :style="{
+        backgroundImage: `url(${moreBenefit.image})`
+      }"
+      :key="moreBenefit.name"
+      class="more-benefit w-[48%] h-[110px] bg-white bg-no-repeat bg-contain bg-[95%_100%] px-4 py-[12px] rounded-xl font-semibold"
+      style="background-size: 57px"
+    >
+      <span>{{ moreBenefit.name }}</span>
+    </div>
+  </div>
+</template>
