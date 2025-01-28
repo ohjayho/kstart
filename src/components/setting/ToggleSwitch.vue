076c9bdcf15771b10b816202477111930b3090a4
Jay Ho Oh <ohjayhoi@gmail.com>
2025-01-28 22:50:04 +0900

    feat: add toggle switch component for setting view

---
+++ src/components/setting/ToggleSwitch.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from "vue";
+
+const isPushOn = ref(false);
+const onToggle = () => {
+  isPushOn.value = !isPushOn.value;
+  console.log(isPushOn.value);
+};
+</script>
+
+<template>
+  <button
+    class="toggle-wrapper w-[38px] h-[20px] rounded-xl border-2 border-t-2 border-b-2 flex items-center relative transition-all"
+    :class="
+      isPushOn
+        ? 'bg-[#2760ed] border-[#2760ed]'
+        : 'bg-[#dcdfe6] border-[#dcdfe6]'
+    "
+    @click="onToggle"
+  >
+    <div
+      class="toggle-switch w-4 h-4 bg-white rounded-full absolute transition-all"
+      :class="isPushOn ? 'translate-x-[18px]' : 'translateX-0'"
+    ></div>
+  </button>
+</template>
