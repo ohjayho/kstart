b46d7a366b096752ce9b570115ab5f2a4275799c
Jay Ho Oh <ohjayhoi@gmail.com>
2025-01-28 22:48:39 +0900

    refactor: add benefit comment component

---
+++ src/components/benefit/BenefitComment.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="benefit-comment flex flex-col items-center mt-10 gap-2">
+    <h5 class="text-xl font-semibold">
+      <span class="text-[#2760ee]">더 많은 혜택</span>을 준비하고 있습니다.
+    </h5>
+    <p class="flex text-center text-[#414549] font-semibold">
+      쇼핑, 커뮤니티, 보험, 대출 서비스에서도<br />
+      더 큰 혜택과 다양한 서비스로 <br />
+      여러분에게 힘이 되겠습니다.
+    </p>
+  </div>
+</template>
--- src/views/BenefitView.vue
+++ src/views/BenefitView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Benefit from "@/components/benefit/Benefit.vue";
+import BenefitComment from "@/components/benefit/BenefitComment.vue";
 import BenefitHeader from "@/components/benefit/BenefitHeader.vue";
 import BenefitImage from "@/components/benefit/BenefitImage.vue";
 import MoreBenefit from "@/components/benefit/MoreBenefit.vue";
@@ -14,16 +15,7 @@ import Header from "@/components/Header.vue";
     >
       <BenefitHeader />
       <Benefit />
-      <div class="benefit-footer flex flex-col items-center mt-10 gap-2">
-        <h5 class="text-xl font-semibold">
-          <span class="text-[#2760ee]">더 많은 혜택</span>을 준비하고 있습니다.
-        </h5>
-        <p class="flex text-center text-[#414549] font-semibold">
-          쇼핑, 커뮤니티, 보험, 대출 서비스에서도<br />
-          더 큰 혜택과 다양한 서비스로 <br />
-          여러분에게 힘이 되겠습니다.
-        </p>
-      </div>
+      <BenefitComment />
       <MoreBenefit />
       <BenefitImage />
     </div>
