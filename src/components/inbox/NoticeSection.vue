efceb98d43ba3b2968d065c485a87aad4228ea23
Jay Ho Oh <ohjayhoi@gmail.com>
2025-01-28 22:49:30 +0900

    feat: add notice section in inbox view

---
+++ src/components/inbox/NoticeSection.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="message-container flex flex-col pt-7">
+    <div class="message-main flex mb-2">
+      <div
+        class="message-icon flex-shrink-0 rounded-full border border-[#2760ed] w-10 h-10 flex justify-center items-center mr-2"
+      >
+        <img
+          src="@/assets/inbox/icon-notice.png"
+          alt="icon-notice"
+          class="w-6"
+        />
+      </div>
+      <div class="message-text text-sm text-[#1f2937]">
+        오재호님의 한국 생활은 어떤가요? 지금 커뮤니티에서 오재호님의 다양한
+        한국 생활 경험을 나누어 보세요.
+      </div>
+    </div>
+    <div class="message-sub flex flex-row-reverse">
+      <div class="message-date text-xs text-[#9ca3af]">
+        2025-01-02 12시 10분
+      </div>
+    </div>
+  </div>
+</template>
