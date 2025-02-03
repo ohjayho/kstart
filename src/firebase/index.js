5f20ea19665126df42ab7ccc6c6481d1de7fcd59
Jay Ho Oh <ohjayhoi@gmail.com>
2025-02-03 16:33:15 +0900

    feat: add firebase config files

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -6,18 +6,18 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDhQ3sDeIUNpexXoUISwct2gKW9DjZc1OU",
-  authDomain: "kstart-1f019.firebaseapp.com",
-  projectId: "kstart-1f019",
-  storageBucket: "kstart-1f019.firebasestorage.app",
-  messagingSenderId: "232338996260",
-  appId: "1:232338996260:web:279dd74d4461ca3b5bacb1"
+  apiKey: VUE_APP_API_KEY,
+  authDomain: VUE_APP_AUTH_DOMAIN,
+  projectId: VUE_APP_PROJECT_ID,
+  storageBucket: VUE_APP_STORAGE_BUCKET,
+  messagingSenderId: VUE_APP_MESSAGING_SENDER_ID,
+  appId: VUE_APP_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Cloud Firestore and get a reference to the service
-const db = getFirestore(app);
+// // Initialize Cloud Firestore and get a reference to the service
+// const db = getFirestore(app);
 
-export { db };
+// export { db };
