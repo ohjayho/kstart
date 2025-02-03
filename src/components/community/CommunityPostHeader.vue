b8b4d811a44f2be4cfd9a879478d7c033bf37bb7
Jay Ho Oh <ohjayhoi@gmail.com>
2025-02-03 17:20:59 +0900

    style: add dots button to community post header

--- src/components/community/CommunityPostHeader.vue
+++ src/components/community/CommunityPostHeader.vue
@@ -8,9 +8,14 @@ const goBack = () => {
 </script>
 
 <template>
-  <header class="sticky top-0 w-full h-15 px-5 py-[11px] bg-white">
+  <header
+    class="sticky top-0 w-full flex justify-between h-15 px-5 py-[11px] bg-white"
+  >
     <button @click="goBack">
       <img src="/img/leftArrow.png" alt="left arrow" class="h-6" />
+    </button>
+    <button>
+      <img src="/img/dots.svg" alt="dots" class="h-6" />
     </button>
   </header>
 </template>
