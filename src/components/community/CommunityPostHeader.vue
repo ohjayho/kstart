11ba8c229b1199d92ce8c3a5c13f93525aedbce4
Jay Ho Oh <ohjayhoi@gmail.com>
2025-02-03 12:51:09 +0900

    feat: add community post header

---
+++ src/components/community/CommunityPostHeader.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const goBack = () => {
+  router.go(-1);
+};
+</script>
+
+<template>
+  <header class="sticky top-0 w-full h-15 px-5 py-[11px] bg-white">
+    <button @click="goBack">
+      <img src="@/assets/img/leftArrow.png" alt="left arrow" class="h-6" />
+    </button>
+  </header>
+</template>
