0fb7b9de14b04a82434bcf027a234d7e53bd6ce3
Jay Ho Oh <ohjayhoi@gmail.com>
2025-02-03 12:23:49 +0900

    feat: add inbox sections component

---
+++ src/components/inbox/InboxNoticeSection.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="message-container flex flex-col pt-7">
+    <div class="message-main flex mb-2">
+      <div
+        class="message-icon flex-shrink-0 rounded-full border border-[#2760ed] w-10 h-10 flex justify-center items-center mr-2"
+      >
+        <img
+          src="@/assets/inbox/icon-notice.png"
+          alt="icon-notice"
+          class="w-6"
+        />
+      </div>
+      <div class="message-text text-sm text-[#1f2937]">
+        Photo Contest 이벤트가 진행 중이에요! 한국의 겨울 사진을 올리고 5만 원을
+        받아 가세요!
+      </div>
+    </div>
+    <div class="message-sub flex flex-row-reverse">
+      <div class="message-date text-xs text-[#9ca3af]">
+        2025-01-02 13시 17분
+      </div>
+    </div>
+  </div>
+</template>
