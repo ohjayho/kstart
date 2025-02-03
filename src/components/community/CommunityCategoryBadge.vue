7191765d92a278050adb85837e25c44286b05597
Jay Ho Oh <ohjayhoi@gmail.com>
2025-02-03 12:21:53 +0900

    feat: add community category badge component for reusability

---
+++ src/components/community/CommunityCategoryBadge.vue
@@ -0,0 +1,22 @@
+<script setup>
+defineProps({
+  text: String
+});
+
+const colors = {
+  All: "#2760ee",
+  "Photo Contest": "#4f97d1",
+  News: "#e67e22",
+  "Life Tip": "#2ecc71",
+  "Q&A": "#8e44ad",
+  "Free Talk": "#f39c12"
+};
+</script>
+
+<template>
+  <span
+    class="category-badge text-white rounded-md text-xs px-1 py-0.5"
+    :style="{ backgroundColor: colors[text] }"
+    >{{ text }}</span
+  >
+</template>
