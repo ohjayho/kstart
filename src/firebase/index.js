66603c72b7bfb93f70c0050cca7d0b7136a50b9a
Jay Ho Oh <ohjayhoi@gmail.com>
2025-02-14 23:40:56 +0900

    fix: change firebase configuration

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -5,17 +5,18 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: VUE_APP_API_KEY,
-  authDomain: VUE_APP_AUTH_DOMAIN,
-  projectId: VUE_APP_PROJECT_ID,
-  storageBucket: VUE_APP_STORAGE_BUCKET,
-  messagingSenderId: VUE_APP_MESSAGING_SENDER_ID,
-  appId: VUE_APP_APP_ID
+export const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
+export const firebase = initializeApp(firebaseConfig);
 
 // // Initialize Cloud Firestore and get a reference to the service
 // const db = getFirestore(app);
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { firebase } from "./firebase";
 
 const app = createApp(App);
 app.use(router);
