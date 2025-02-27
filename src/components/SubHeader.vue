0d9d6a27fa50806209802e163d4bd6c10c5423e1
Jay Ho Oh <ohjayhoi@gmail.com>
2025-02-27 14:41:36 +0900

    feat: add sub header component for pages

---
+++ src/components/SubHeader.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { useRouter } from "vue-router";
+
+defineProps({
+  title: String
+});
+
+const router = useRouter();
+const goBack = () => {
+  router.back();
+};
+</script>
+
+<template>
+  <header
+    class="w-[500px] max-w-full h-[60px] fixed top-0 z-20 flex justify-center items-center px-5 bg-white"
+  >
+    <button @click="goBack" class="absolute left-6">
+      <img src="/img/leftArrow.png" alt="" class="h-6" />
+    </button>
+    <h1 class="text-lg font-bold">{{ title }}</h1>
+  </header>
+</template>
