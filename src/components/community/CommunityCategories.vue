f2547706737283b932ddf3746de6c1589040d321
Jay Ho Oh <ohjayhoi@gmail.com>
2025-03-02 23:01:11 +0900

    refactor: add template tag to use category variable in v-if directive

--- src/components/community/CommunityCategories.vue
+++ src/components/community/CommunityCategories.vue
@@ -3,7 +3,7 @@ import { ref } from "vue";
 import NewBadge from "../NewBadge.vue";
 
 defineProps({
-  isBadgeVisible: Boolean
+  newPost: Boolean
 });
 
 const categories = [
@@ -24,28 +24,24 @@ const onClick = (category) => {
 </script>
 
 <template>
-  <div class="categories-container $attrs.class flex gap-2 flex-wrap p-4">
-    <button
-      v-for="category in categories"
-      :key="category"
-      class="text-[13px] border px-5 py-2 rounded-2xl relative"
-      :class="
-        category === curCategory
-          ? 'bg-[#2760ee] text-white border-[#C6E2FF]'
-          : 'bg-white text-[#6d7280] border-[#e6e8ed]'
-      "
-      @click="onClick(category)"
-    >
-      {{ category }}
-      <NewBadge
-        v-if="category === 'Photo Contest' && isBadgeVisible"
-        class="-left-1 w-5 h-5"
-      />
-    </button>
-    <div class="posts-container">
-      <div class="post-header"></div>
-      <h1 class="post-title"></h1>
-      <div class="post-footer"></div>
-    </div>
+  <div class="categories-container $attrs.class flex gap-2 flex-wrap py-4">
+    <template v-for="category in categories" :key="category">
+      <button
+        v-if="!(category === 'All' && newPost)"
+        class="text-[13px] border px-5 py-2 rounded-2xl relative"
+        :class="
+          category === curCategory
+            ? 'bg-[#2760ee] text-white border-[#C6E2FF]'
+            : 'bg-white text-[#6d7280] border-[#e6e8ed]'
+        "
+        @click="onClick(category)"
+      >
+        {{ category }}
+        <NewBadge
+          v-if="category === 'Photo Contest' && !newPost"
+          class="-left-1 w-5 h-5"
+        />
+      </button>
+    </template>
   </div>
 </template>
