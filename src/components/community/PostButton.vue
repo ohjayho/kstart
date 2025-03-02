76e4d3367cfc57894eec3dcc6e6331c6091c1bda
Jay Ho Oh <ohjayhoi@gmail.com>
2025-03-02 23:02:51 +0900

    feat: add new post button

--- src/components/community/PostButton.vue
+++ src/components/community/PostButton.vue
@@ -1,16 +1,24 @@
 <script setup>
+import { useAuth } from "@/firebase/checkAuth";
 import { useRouter } from "vue-router";
 const router = useRouter();
+const { isLoggedIn } = useAuth();
 
 const addPost = () => {
-  router.push("/post");
+  if (!isLoggedIn.value) {
+    alert("로그인 해주세요.");
+    router.push("/login");
+  } else {
+    router.push("/post");
+  }
 };
 </script>
 
 <template>
   <button
     @click="addPost"
-    class="w-[52px] h-[52px] rounded-full bg-[#2760ee] px-2 py-[15px] sticky top-0"
+    class="w-[52px] h-[52px] flex justify-center items-center text-[25px] text-white rounded-full bg-[#2760ee] px-2 py-[15px] fixed bottom-[80px]"
+    :style="{ right: 'calc((100vw - min(100vw,500px))/2 + 5px' }"
   >
     +
   </button>
