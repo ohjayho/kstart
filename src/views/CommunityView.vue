9abf86c9101c9e310c03e4385d71139a270e6484
Jay Ho Oh <ohjayhoi@gmail.com>
2025-03-03 00:57:41 +0900

    feat: read posts from firebase in real time

--- src/views/CommunityView.vue
+++ src/views/CommunityView.vue
@@ -3,14 +3,37 @@ import CommunityCategories from "@/components/community/CommunityCategories.vue"
 import CommunityPosts from "@/components/community/CommunityPosts.vue";
 import SearchBar from "@/components/community/SearchBar.vue";
 import postsData from "@/assets/postsData.json";
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
 import PostButton from "@/components/community/PostButton.vue";
+import { db } from "@/firebase/initializeFirebase";
+import { collection, onSnapshot } from "firebase/firestore";
 
 const posts = ref(postsData);
 const curCategory = ref("All");
 const handleUpdateCategory = (category) => {
   curCategory.value = category;
 };
+
+onMounted(() => {
+  onSnapshot(collection(db, "posts"), (querySnapshot) => {
+    const fbPosts = [];
+    querySnapshot.forEach((doc) => {
+      const post = {
+        user: doc.data().user,
+        date: doc.data().date,
+        category: doc.data().category,
+        title: doc.data().title,
+        description: doc.data().description,
+        img: doc.data().img,
+        likes: doc.data().likes,
+        comments: doc.data().comments
+      };
+      fbPosts.push(post);
+      console.log(doc.id, "=>", doc.data());
+    });
+    posts.value = fbPosts;
+  });
+});
 </script>
 
 <template>
