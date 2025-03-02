49179c8b1d10c8666507fc877949e8f47fd73a26
Jay Ho Oh <ohjayhoi@gmail.com>
2025-03-03 00:55:21 +0900

    feat: add a format time util

---
+++ src/utils/formatTimeAgo.js
@@ -0,0 +1,23 @@
+export function formatTimeAgo(fbSeconds) {
+  if (!fbSeconds) {
+    return "No data available";
+  }
+  const date = new Date(fbSeconds * 1000);
+  //Date function takes milliseconds
+  const now = new Date();
+  console.log(date, now);
+  const seconds = Math.floor((now - date) / 1000);
+
+  if (seconds < 60) {
+    return `${seconds} seconds ago`;
+  } else if (seconds < 3600) {
+    const minutes = Math.floor(seconds / 60);
+    return `${minutes} minutes ago`;
+  } else if (seconds < 86400) {
+    const hours = Math.floor(seconds / 3600);
+    return `${hours} hours ago`;
+  } else {
+    const days = Math.floor(seconds / 86400);
+    return `${days} days ago`;
+  }
+}
