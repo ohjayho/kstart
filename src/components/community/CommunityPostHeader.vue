9e837629159463aeb9877835c0855525a9b473c6
Jay Ho Oh <ohjayhoi@gmail.com>
2025-03-12 00:19:01 +0900

    feat: add post detail view popup

--- src/components/community/CommunityPostHeader.vue
+++ src/components/community/CommunityPostHeader.vue
@@ -1,10 +1,44 @@
 <script setup>
+import { onMounted, onUnmounted, ref } from "vue";
 import { useRouter } from "vue-router";
+import CommunityPostPopup from "./CommunityPostPopup.vue";
+
+defineProps({
+  postId: String
+});
 
 const router = useRouter();
 const goBack = () => {
   router.go(-1);
 };
+
+const popupOpen = ref(false);
+
+const handleClickPopup = () => {
+  popupOpen.value = true;
+  activePopup.value = true;
+  console.log("helo");
+};
+
+const activePopup = ref(null);
+
+const handleClickOutside = (e) => {
+  if (activePopup.value !== null) {
+    const popupElement = document.querySelector(".post-detail-popup");
+    if (popupElement && !popupElement.contains(e.target)) {
+      popupOpen.value = false;
+      activePopup.value = null;
+    }
+  }
+};
+
+onMounted(() => {
+  document.addEventListener("click", handleClickOutside);
+});
+
+onUnmounted(() => {
+  document.removeEventListener("click", handleClickOutside);
+});
 </script>
 
 <template>
@@ -14,8 +48,9 @@ const goBack = () => {
     <button @click="goBack">
       <img src="/img/leftArrow.png" alt="left arrow" class="h-6" />
     </button>
-    <button>
+    <button @click="handleClickPopup" class="post-detail-popup relative">
       <img src="/img/dots.svg" alt="dots" class="h-6" />
+      <CommunityPostPopup v-if="popupOpen" :postId="postId" />
     </button>
   </header>
 </template>
